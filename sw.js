a286e10be6e56b1d11a3fc05b903ca1cd24fe663
HowlHouse <howlhousemedia@gmail.com>
2026-09-08 17:48:33 -0700

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('Service Worker: Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Allows the app to bypass basic fetch requirements for PWA installation
-});
